8a1905c9feca73cadd0d4674b06fce7e365292dc
Kevin Minehart <kmineh0151@gmail.com>
2020-08-14 17:11:04 -0500

    Remove revoked token

--- pkg/models/settings.go
+++ pkg/models/settings.go
@@ -11,14 +11,12 @@ type Settings struct {
 	AccessToken string
 }
 
-// LLoadSettings converts the DataSourceInLoadSettings to usable Github settings
+// LoadSettings converts the DataSourceInLoadSettings to usable Github settings
 func LoadSettings(settings backend.DataSourceInstanceSettings) (Settings, error) {
 	s := Settings{}
 	if err := json.Unmarshal(settings.JSONData, &s); err != nil {
 		return Settings{}, err
 	}
 
-	// s.AccessToken = settings.DecryptedSecureJSONData["access_token"]
-	s.AccessToken = "8235020a22677db9b33c43fd29c9d10800b203eb"
 	return s, nil
 }
