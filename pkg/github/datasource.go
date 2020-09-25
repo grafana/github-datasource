ec6dc858ce1cc4878b62603e2eff7582bfcbdf4f
Kevin Minehart <kmineh0151@gmail.com>
2020-09-24 21:03:30 -0500

    githubUrl -> githubURL

--- pkg/github/datasource.go
+++ pkg/github/datasource.go
@@ -3,6 +3,7 @@ package github
 import (
 	"context"
 	"fmt"
+
 	"github.com/grafana/github-datasource/pkg/dfutil"
 	"github.com/grafana/github-datasource/pkg/models"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
@@ -136,13 +137,13 @@ func NewDatasource(ctx context.Context, settings models.Settings) *Datasource {
 
 	httpClient := oauth2.NewClient(ctx, src)
 
-	if settings.GithubUrl == "" {
+	if settings.GithubURL == "" {
 		return &Datasource{
 			client: githubv4.NewClient(httpClient),
 		}
 	}
 
 	return &Datasource{
-		client: githubv4.NewEnterpriseClient(fmt.Sprintf("%s/api/graphql", settings.GithubUrl), httpClient),
+		client: githubv4.NewEnterpriseClient(fmt.Sprintf("%s/api/graphql", settings.GithubURL), httpClient),
 	}
 }
--- pkg/models/settings.go
+++ pkg/models/settings.go
@@ -9,7 +9,7 @@ import (
 // Settings represents the Datasource options in Grafana
 type Settings struct {
 	AccessToken    string `json:"accessToken"`
-	GithubUrl      string `json:"githubUrl"`
+	GithubURL      string `json:"githubUrl"`
 	CachingEnabled bool   `json:"cachingEnabled"`
 }
 
