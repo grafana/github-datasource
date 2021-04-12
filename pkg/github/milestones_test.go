e1e4d112616f746e08e5d8b9377a84b70b0f49bd
Kevin Minehart <kmineh0151@gmail.com>
2021-04-12 08:25:07 -0500

    Fix loop pointers in pull requests, releases, and milestones (#124)
    * fix loop pointer errors for pull requests and milestones
    
    * fix loop pointer error for releases

--- pkg/github/milestones_test.go
+++ pkg/github/milestones_test.go
@@ -3,9 +3,11 @@ package github
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/grafana/github-datasource/pkg/models"
 	"github.com/grafana/github-datasource/pkg/testutil"
+	"github.com/shurcooL/githubv4"
 )
 
 func TestListMilestones(t *testing.T) {
@@ -27,6 +29,88 @@ func TestListMilestones(t *testing.T) {
 
 	_, err := GetAllMilestones(ctx, client, opts)
 	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMilestonesDataframe(t *testing.T) {
+	openedAt, err := time.Parse(time.RFC3339, "2020-08-25T16:21:56+00:00")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	firstUser := User{
+		ID:      "1",
+		Login:   "testUser",
+		Name:    "Test User",
+		Company: "ACME corp",
+		Email:   "user@example.com",
+	}
+	secondUser := User{
+		ID:      "2",
+		Login:   "testUser2",
+		Name:    "Second User",
+		Company: "ACME corp",
+		Email:   "user2@example.com",
+	}
+
+	milestones := Milestones{
+		{
+			Closed: false,
+			Creator: struct {
+				User User "graphql:\"... on User\""
+			}{
+				User: firstUser,
+			},
+			DueOn: githubv4.DateTime{
+				Time: openedAt.Add(100 * time.Hour),
+			},
+			ClosedAt: githubv4.DateTime{},
+			CreatedAt: githubv4.DateTime{
+				Time: openedAt,
+			},
+			State: githubv4.MilestoneStateOpen,
+			Title: "first milestone",
+		},
+		{
+			Closed: true,
+			Creator: struct {
+				User User "graphql:\"... on User\""
+			}{
+				User: secondUser,
+			},
+			DueOn: githubv4.DateTime{
+				Time: openedAt.Add(100 * time.Hour),
+			},
+			ClosedAt: githubv4.DateTime{
+				Time: openedAt.Add(10 * time.Hour),
+			},
+			CreatedAt: githubv4.DateTime{
+				Time: openedAt,
+			},
+			State: githubv4.MilestoneStateClosed,
+			Title: "seoncd milestone",
+		},
+		{
+			Closed: false,
+			Creator: struct {
+				User User "graphql:\"... on User\""
+			}{
+				User: secondUser,
+			},
+			DueOn: githubv4.DateTime{
+				Time: openedAt.Add(120 * time.Hour),
+			},
+			ClosedAt: githubv4.DateTime{},
+			CreatedAt: githubv4.DateTime{
+				Time: openedAt,
+			},
+			State: githubv4.MilestoneStateOpen,
+			Title: "third milestone",
+		},
+	}
+
+	if err := testutil.CheckGoldenFramer("milestones", milestones); err != nil {
 		t.Fatal(err)
 	}
 }
