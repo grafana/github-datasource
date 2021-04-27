f359c2ee152c754ac4f55a1ac618a9cd9de1321f
Scott Lepper <scott.lepper@gmail.com>
2021-04-27 08:30:01 -0400

    fix e2e

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,7 +12,7 @@ echo "starting tests"
 export CYPRESS_HOST=host.docker.internal
 export CYPRESS_BASE_URL=http://host.docker.internal:3000
 
-yarn e2e
+yarn e2e:update
 
 # this will simulate how it runs on ci
 # wget -O grabpl https://grafana-downloads.storage.googleapis.com/grafana-build-pipeline/v0.5.40/grabpl
