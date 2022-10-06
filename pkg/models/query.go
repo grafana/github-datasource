0d41696d2bd3237f8d2bcdfaac904547be804126
Tomas Dabasinskas <tomas@dabasinskas.net>
2022-10-06 15:07:58 +0300

    Fix incorrectly typed query options
    For some reason (copy-paste?) we are using `ListMilestonesOptions` for vulnerabilities — we should be using `ListVulnerabilitiesOptions` instead.

--- pkg/models/query.go
+++ pkg/models/query.go
@@ -100,5 +100,5 @@ type MilestonesQuery struct {
 // VulnerabilityQuery is used when querying for GitHub Repository Vulnerabilities
 type VulnerabilityQuery struct {
 	Query
-	Options ListMilestonesOptions `json:"options"`
+	Options ListVulnerabilitiesOptions `json:"options"`
 }
