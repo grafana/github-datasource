7a5029e7b42e6e3e82946f9f63148be4c2c108a7
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2022-10-12 15:07:23 +0100

    made e2e work (partially working now)

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -1,115 +1,52 @@
 import { e2e } from '@grafana/e2e';
-import { PartialConfigurePanelConfig } from '@grafana/e2e/flows/configurePanel';
 import { selectors } from '../../src/components/selectors';
+import { openDashboardSettings } from './utils';
 
 const e2eSelectors = e2e.getSelectors(selectors.components);
 
 const addGithubDataSource = (accessToken: string) => {
   return e2e.flows.addDataSource({
-    checkHealth: true,
     expectedAlertMessage: 'OK',
-    form: () => {
-      e2eSelectors.ConfigEditor.AccessToken.input()
-        .scrollIntoView()
-        .type(accessToken);
-    },
+    form: () => e2eSelectors.ConfigEditor.AccessToken.input().scrollIntoView().type(accessToken),
+    timeout: 10000,
     type: 'GitHub',
   });
 };
 
-const addGithubPanel = (variableName: string) => {
-  const fillQueryEditor = () => {
-    // Fill in the Query Editor with data needed
-    // We are specifically looking at Releases with `grafana` repo to ensure we don't have any
-    // sensitive info that will be caught by the screenshot
-    e2e.components.QueryEditorRows.rows()
-      .should('be.visible')
-      .within(() => {
-        e2e.components.Select.input()
-          .first()
-          .should('be.empty')
-          .focus()
-          .type(`Releases{enter}`);
-      });
-
-    e2eSelectors.QueryEditor.Owner.input()
-      .should('be.empty')
-      .type(`grafana{enter}`);
-    e2eSelectors.QueryEditor.Repository.input()
-      .should('be.empty')
-      .type(`grafana{enter}`);
-  };
-
-  e2e.flows
-    .addPanel({
-      matchScreenshot: true,
-      queriesForm: () => {
-        fillQueryEditor();
-
-        // Switch to the Table view as we have tabular data from Github (for the screenshot)
-        e2e.components.PanelEditor.OptionsPane.open()
-          .should('be.visible')
-          .click();
-        e2e.components.OptionsGroup.toggle('Panel type')
-          .should('be.visible')
-          .click();
-        e2e.components.PluginVisualization.item('Table')
-          .scrollIntoView()
-          .should('be.visible')
-          .click();
-      },
-    })
-    .then(({ config: { panelTitle } }: { config: PartialConfigurePanelConfig }) => {
-      // Make sure the template variable works
-      e2e.flows.editPanel({
-        matchScreenshot: true,
-        panelTitle,
-        queriesForm: () => {
-          e2eSelectors.QueryEditor.Owner.input()
-            .clear()
-            .type(`$${variableName}{enter}`);
-        },
-        visitDashboardAtStart: false,
-      });
-    });
-};
-
-e2e.scenario({
-  describeName: 'Smoke tests',
-  itName: 'Login, create data source, annotation, template variable, dashboard, panel',
-  scenario: () => {
+describe('Smoke test', () => {
+  it('should render default variable editor', () => {
+    e2e.flows.login();
     e2e()
-      .readProvisions([
-        // Paths are relative to <project-root>/provisioning
-        'datasources/github.yaml',
-      ])
+      .readProvisions(['datasources/github.yaml'])
       .then(([provision]) => addGithubDataSource(provision.datasources[0].secureJsonData.accessToken))
       .then(({ config: { name: dataSourceName } }) => {
-        const variableName = 'owner';
-
-        e2e.flows.addDashboard({
-          annotations: [
-            {
-              dataSource: dataSourceName,
-              dataSourceForm: () => e2eSelectors.AnnotationEditor.container().should('be.visible'),
-              name: 'Annotations',
+        e2e.flows
+          .addDashboard({
+            timeRange: {
+              from: '2020-12-01 00:00:00',
+              to: '2020-12-31 23:59:59',
             },
-          ],
-          timeRange: {
-            from: '2020-12-01 00:00:00',
-            to: '2020-12-31 23:59:59',
-          },
-          variables: [
-            {
-              constantValue: 'grafana',
-              label: 'Template Variable',
-              name: variableName,
-              type: e2e.flows.VARIABLE_TYPE_CONSTANT,
-            },
-          ],
-        });
-
-        addGithubPanel(variableName);
+          })
+          .then(() => {
+            const variableName = 'owner';
+            openDashboardSettings('Variables');
+            e2e.pages.Dashboard.Settings.Variables.List.addVariableCTAV2().click();
+            e2e.pages.Dashboard.Settings.Variables.Edit.General.generalNameInput().clear().type(variableName);
+            cy.wait(6 * 1000); // When clearing the variable name, the validation popup comes and hides the datasource picker. so wait sometime till the popup closes.
+            e2e.pages.Dashboard.Settings.Variables.Edit.General.generalTypeSelect().type('Constant{enter}');
+            e2e.pages.Dashboard.Settings.Variables.Edit.ConstantVariable.constantOptionsQueryInput()
+              .type('grafana')
+              .blur();
+            e2e.pages.Dashboard.Settings.Variables.Edit.General.previewOfValuesOption()
+              .eq(0)
+              .should('have.text', 'grafana');
+            e2e.pages.Dashboard.Settings.Variables.Edit.General.submitButton().click();
+            e2e.components.BackButton.backArrow().click({ force: true });
+            e2e.components.RefreshPicker.runButtonV2().click();
+            // e2e.flows.addPanel({
+            //   queriesForm: () => {},
+            // });
+          });
       });
-  },
+  });
 });
