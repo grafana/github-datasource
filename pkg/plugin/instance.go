7b28df6275174a7c4ba17848ad1f34a0dceb401d
Scott Lepper <scott.lepper@gmail.com>
2022-10-20 12:33:02 -0400

    cleanup

--- pkg/plugin/instance.go
+++ pkg/plugin/instance.go
@@ -9,77 +9,6 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 )
 
-// Instance is the root Datasource implementation that wraps a Datasource
-// type Instance struct {
-// 	Datasource Datasource
-// 	Handlers   Handlers
-// }
-
-// // HandleRepositoriesQuery ...
-// func (i *Instance) HandleRepositoriesQuery(ctx context.Context, q *models.RepositoriesQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleRepositoriesQuery(ctx, q, req)
-// }
-
-// // HandleIssuesQuery ...
-// func (i *Instance) HandleIssuesQuery(ctx context.Context, q *models.IssuesQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleIssuesQuery(ctx, q, req)
-// }
-
-// // HandleCommitsQuery ...
-// func (i *Instance) HandleCommitsQuery(ctx context.Context, q *models.CommitsQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleCommitsQuery(ctx, q, req)
-// }
-
-// // HandleTagsQuery ...
-// func (i *Instance) HandleTagsQuery(ctx context.Context, q *models.TagsQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleTagsQuery(ctx, q, req)
-// }
-
-// // HandleReleasesQuery ...
-// func (i *Instance) HandleReleasesQuery(ctx context.Context, q *models.ReleasesQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleReleasesQuery(ctx, q, req)
-// }
-
-// // HandleContributorsQuery ...
-// func (i *Instance) HandleContributorsQuery(ctx context.Context, q *models.ContributorsQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleContributorsQuery(ctx, q, req)
-// }
-
-// // HandlePullRequestsQuery ...
-// func (i *Instance) HandlePullRequestsQuery(ctx context.Context, q *models.PullRequestsQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandlePullRequestsQuery(ctx, q, req)
-// }
-
-// // HandleLabelsQuery ...
-// func (i *Instance) HandleLabelsQuery(ctx context.Context, q *models.LabelsQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleLabelsQuery(ctx, q, req)
-// }
-
-// // HandlePackagesQuery ...
-// func (i *Instance) HandlePackagesQuery(ctx context.Context, q *models.PackagesQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandlePackagesQuery(ctx, q, req)
-// }
-
-// // HandleMilestonesQuery ...
-// func (i *Instance) HandleMilestonesQuery(ctx context.Context, q *models.MilestonesQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleMilestonesQuery(ctx, q, req)
-// }
-
-// // HandleVulnerabilitiesQuery ...
-// func (i *Instance) HandleVulnerabilitiesQuery(ctx context.Context, q *models.VulnerabilityQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleVulnerabilitiesQuery(ctx, q, req)
-// }
-
-// // HandleProjectsQuery ...
-// func (i *Instance) HandleProjectsQuery(ctx context.Context, q *models.ProjectsQuery, req backend.DataQuery) (dfutil.Framer, error) {
-// 	return i.Datasource.HandleProjectsQuery(ctx, q, req)
-// }
-
-// // CheckHealth ...
-// func (i *Instance) CheckHealth(ctx context.Context) error {
-// 	return i.Datasource.CheckHealth(ctx)
-// }
-
 // NewGitHubInstance creates a new GitHubInstance using the settings to determine if things like the Caching Wrapper should be enabled
 func NewGitHubInstance(ctx context.Context, settings models.Settings) instancemgmt.Instance {
 	var (
@@ -92,14 +21,6 @@ func NewGitHubInstance(ctx context.Context, settings models.Settings) instancemg
 		d = WithCaching(d)
 	}
 
-	// TODO: wrap these HTTP handlers with a caching wrapper
-	// return &Instance{
-	// 	Datasource: d,
-	// 	Handlers: Handlers{
-	// 		Labels:     gh.HandleGetLabels,
-	// 		Milestones: gh.HandleGetMilestones,
-	// 	},
-	// }
 	return d
 }
 
