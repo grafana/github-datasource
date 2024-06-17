5177ebc0321d6e5f565a0efb3b93bc1d49dc0fdd
Ivana Huckova <ivana.huckova@gmail.com>
2024-06-17 13:50:07 +0200

    Add more tests

--- src/DataSource.test.ts
+++ src/DataSource.test.ts
@@ -1,4 +1,4 @@
-import { DataSourceInstanceSettings } from "@grafana/data";
+import { DataQueryResponse, DataSourceInstanceSettings, toDataFrame } from "@grafana/data";
 import { GitHubVariableQuery } from "types";
 import { of } from 'rxjs';
 import { GithubDataSource } from "DataSource";
@@ -12,6 +12,26 @@ describe('DataSource', () => {
       jest.spyOn(ds, 'query').mockReturnValue(of({data: []}))
       const res = await ds.metricFindQuery(query, {})
       expect(res).toEqual([])
+    });
+
+    it('should return empty array if no data in response', async () => {
+      const ds = new GithubDataSource({} as DataSourceInstanceSettings);
+      const query = {} as GitHubVariableQuery
+
+      jest.spyOn(ds, 'query').mockReturnValue(of({} as DataQueryResponse))
+      const res = await ds.metricFindQuery(query, {})
+      expect(res).toEqual([])
+    });
+
+    it('should return array with values if response has data', async () => {
+      const ds = new GithubDataSource({} as DataSourceInstanceSettings);
+      const query = {key: 'test', field: 'test'} as GitHubVariableQuery
+
+      jest.spyOn(ds, 'query').mockReturnValue(of({data: [toDataFrame({
+        fields: [{name: 'test', values: ['value1', 'value2']}]
+      })]} as DataQueryResponse))
+      const res = await ds.metricFindQuery(query, {})
+      expect(res).toEqual([{value: 'value1', text: 'value1'}, {value: 'value2', text: 'value2'}])
     });
     })
   }); 
