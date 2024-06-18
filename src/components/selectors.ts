e81c7200d9a8c0f2d7fedaeedca5154b8424bf4a
Ivana Huckova <ivana.huckova@gmail.com>
2024-06-18 14:07:14 +0200

    fix lint

--- src/components/selectors.ts
+++ src/components/selectors.ts
@@ -25,7 +25,7 @@ export const components = {
     },
     Issues: {
       timeFieldInput: 'Query editor issues time field',
-    }
+    },
   },
   AnnotationEditor: {
     container: 'Annotation editor container',
--- src/views/QueryEditorIssues.test.tsx
+++ src/views/QueryEditorIssues.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import QueryEditorIssues from "./QueryEditorIssues";
+import QueryEditorIssues from './QueryEditorIssues';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { components } from 'components/selectors';
@@ -8,12 +8,12 @@ describe('QueryEditorIssues', () => {
   it('should have CreatedAt, ClosedAt and UpdatedAt time field option', async () => {
     const props = {
       onChange: jest.fn(),
-    }
+    };
     render(<QueryEditorIssues {...props} />);
     expect(screen.getByText('Time Field')).toBeInTheDocument();
     userEvent.click(screen.getByTestId(components.QueryEditor.Issues.timeFieldInput));
     expect(await screen.findByText('CreatedAt')).toBeInTheDocument();
     expect(await screen.findByText('ClosedAt')).toBeInTheDocument();
     expect(await screen.findByText('UpdatedAt')).toBeInTheDocument();
-  })
-})
+  });
+});
