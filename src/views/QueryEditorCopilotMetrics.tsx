7519cfd0a25939cdf6f62ee92562c27401b751f4
Austen Stone <austenstone@github.com>
2025-07-10 12:07:26 -0400

    fix ui

--- src/views/QueryEditorCopilotMetrics.tsx
+++ src/views/QueryEditorCopilotMetrics.tsx
@@ -9,10 +9,23 @@ interface Props extends CopilotMetricsOptions {
 }
 
 const QueryEditorCopilotMetrics = (props: Props) => {
-  const [team, setTeam] = useState<string>(props.teamSlug || '');
+  const [team, setTeam] = useState(props.teamSlug || '');
+  const [owner, setOwner] = useState<string>(props.owner || '');
 
   return (
     <>
+      <Input
+        aria-label={components.QueryEditor.Owner.input}
+        width={RightColumnWidth}
+        value={owner}
+        onChange={(el) => setOwner(el.currentTarget.value)}
+        onBlur={(el) =>
+          props.onChange({
+            ...props,
+            owner: el.currentTarget.value,
+          })
+        }
+      />
       <InlineField labelWidth={LeftColumnWidth * 2} label="Team Slug" tooltip="GitHub team slug name (optional - leave empty for organization-wide metrics)">
         <Input
           aria-label={components.QueryEditor.Owner.input}
