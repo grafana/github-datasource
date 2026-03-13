1a8433c31ec569dfd0a49a4df3cc50c62e7ab3a1
yesoreyeram <153843+yesoreyeram@users.noreply.github.com>
2026-03-13 23:19:46 +0000

    strict types for config

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,19 +1,56 @@
-import type { DataSourceJsonData } from '@grafana/data';
+import type { DataSourceJsonData } from '@grafana/schema';
 
 export type GitHubLicenseType = 'github-basic' | 'github-enterprise-cloud' | 'github-enterprise-server';
 
 export type GitHubAuthType = 'personal-access-token' | 'github-app';
 
-export type GitHubDataSourceOptions = {
-  githubPlan?: GitHubLicenseType;
-  githubUrl?: string;
-  selectedAuthType?: GitHubAuthType;
-  appId?: string;
-  installationId?: string;
-} & DataSourceJsonData;
+type GithubCommonOptionsBase<T extends GitHubLicenseType> = {
+  githubPlan?: T;
+} & DataSourceJsonData
 
-export type GitHubSecureJsonDataKeys =
-  | 'accessToken' // accessToken is set if the user is using a Personal Access Token to connect to GitHub
-  | 'privateKey'; // privateKey is set if the user is using a GitHub App to connect to GitHub
+type GitHubDataSourceBasicOptions = GithubCommonOptionsBase<'github-basic'> & {
+  githubPlan?: 'github-basic';
+  githubUrl: never;
+};
 
-export type GitHubSecureJsonData = Partial<Record<GitHubSecureJsonDataKeys, string>>;
+type GitHubDataSourceEnterpriseCloudOptions = GithubCommonOptionsBase<'github-enterprise-cloud'> & {
+  githubPlan: 'github-enterprise-cloud';
+  githubUrl: never;
+};
+
+type GitHubDataSourceEnterpriseServerOptions = GithubCommonOptionsBase<'github-enterprise-server'> & {
+  githubPlan: 'github-enterprise-server';
+  githubUrl: string;
+};
+
+type GithubDataSourceCommonOptions = (GitHubDataSourceBasicOptions | GitHubDataSourceEnterpriseCloudOptions | GitHubDataSourceEnterpriseServerOptions)
+
+type GithubDataSourceAuthOptionsBase<T extends GitHubAuthType> = {
+  selectedAuthType?: T
+}
+
+type GitHubDataSourcePATAuthOptions = GithubDataSourceAuthOptionsBase<'personal-access-token'> & {
+  appId: never;
+  installationId: string;
+};
+
+type GitHubDataSourceGHAppOptions = GithubDataSourceAuthOptionsBase<'github-app'> & {
+  appId: string;
+  installationId: string;
+};
+
+type GithubDataSourceAuthOptions = (GitHubDataSourcePATAuthOptions | GitHubDataSourceGHAppOptions)
+
+export type GitHubDataSourceOptions = GithubDataSourceCommonOptions & GithubDataSourceAuthOptions;
+
+type GitHubSecureJsonDataAuthPAT = {
+  accessToken: string;
+  privateKey?: string;
+};
+
+type GitHubSecureJsonDataAuthGHApp = {
+  privateKey: string;
+  accessToken?: string;
+};
+
+export type GitHubSecureJsonData = GitHubSecureJsonDataAuthPAT | GitHubSecureJsonDataAuthGHApp;
